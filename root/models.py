371e2a1a621d3f6f033e93f81bd1ba1a73348470
Yurij Lyubchenko <lubchenko05@gmail.com>
2017-10-28 18:13:58 +0300

    add cinema and cinemaplaces to models

--- root/models.py
+++ root/models.py
@@ -1,6 +1,11 @@
 import datetime
 
+from django.contrib.auth import get_user_model
 from django.db import models
+from rest_framework.compat import MaxValueValidator, MinValueValidator
+
+
+UserModel = get_user_model()
 
 
 class Film(models.Model):
@@ -21,6 +26,26 @@ class FilmSession(models.Model):
 
     def __str__(self):
         return '%s - %s' % (self.film.name, self.datetime)
+
+
+class Cinema(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def get_free_places(self):
+        return self.places.filter(owner=None)
+
+
+class CinemaPlaces(models.Model):
+    cinema = models.ForeignKey(Cinema, related_name='places')
+    row = models.IntegerField(validators=[MaxValueValidator(100), MinValueValidator(1)])
+    position = models.ImageField(validators=[MaxValueValidator(100), MinValueValidator(1)])
+    owner = models.ForeignKey(UserModel, related_name='places', null=True)
+
+    def __str__(self):
+        return self.cinema.name
 
 
 class FilmPoster(models.Model):
